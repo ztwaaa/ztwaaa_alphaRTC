ac327a3249f81030d6177af9d744371a205df454
六个钢镚 <dz2878898149@163.com>
2022-03-16 19:34:49 +0800

    socket for wsl

---
+++ runtime/ev_socket.h
@@ -0,0 +1,59 @@
+#ifndef __ev_socket_h_
+#define __ev_socket_h_
+
+#include <ev.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ev.h>
+
+struct DataPacket{
+        DataPacket();
+        DataPacket( float RTT,float lost_per_sec,
+                    float loss_rate,float recv_rate,
+                    float retrans_num,float send_rate_last);
+        ~DataPacket() = default;
+        float RTT;
+        float lost_per_sec;
+        float loss_rate;
+        float recv_rate;
+        float retrans_num;
+        float send_rate_last;
+    };
+DataPacket::DataPacket()
+    :   RTT(0),
+        lost_per_sec(0),
+        loss_rate(0),
+        recv_rate(0),
+        retrans_num(0),
+        send_rate_last(0) {}
+
+DataPacket::DataPacket( float RTT,float lost_per_sec,
+                                    float loss_rate,float recv_rate,
+                                    float retrans_num,float send_rate_last)
+    :   RTT(RTT),
+        lost_per_sec(lost_per_sec),
+        loss_rate(loss_rate),
+        recv_rate(recv_rate),
+        retrans_num(retrans_num),
+        send_rate_last(send_rate_last) {}
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*typedef struct ai_params
+{
+	uint32_t		received_bit_rate;
+	int				frameloss;
+	uint32_t		rtt;
+	uint8_t			packet_loss_rate;
+	int				nack_sent_count;
+}ai_model_input_t;*/
+#ifdef __cplusplus
+}
+#endif
+#endif // !__epoll_sender_h_
